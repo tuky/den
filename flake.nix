8341c05da3a413efc80bfddc2c5b3f7e072d1b90
Tobias Krönke <tobias.kroenke@pixolo.com>
2026-08-24 14:25:05 +0200

    fix: seemingly 1st working iteration

--- flake.nix
+++ flake.nix
@@ -11,68 +11,43 @@
   };
 
   outputs = { self, nixpkgs, home-manager, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        lib = nixpkgs.lib;
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
 
-        # Platform detection
-        isDarwin = system == "aarch64-darwin" || system == "x86_64-darwin";
-        isLinux = !isDarwin;
-
-        # Common modules shared across all platforms
-        commonModules = [
-          ./home-manager/home.nix
-        ];
-
-        # Platform-specific modules
-        platformModules =
-          if isDarwin then
-            [ ./hosts/darwin/home.nix ]
-          else
-            [ ./hosts/linux/home.nix ];
-
-        homeModules = commonModules ++ platformModules;
-
-      in
-      {
-        homeConfigurations = {
-          # Generic Linux configuration (for WSL, GCP, etc.)
-          linux = home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            modules = homeModules ++ [
-              {
-                home.username = "tobias";
-                home.homeDirectory =
-                  if isLinux then "/home/tobias" else "/Users/tobias";
-                home.stateVersion = "24.05";
-              }
-            ];
-          };
-
-          # macOS configuration (for when you get a Mac)
-          darwin = home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            modules = homeModules ++ [
-              {
-                home.username = "tobias";
-                home.homeDirectory = "/Users/tobias";
-                home.stateVersion = "24.05";
-              }
-            ];
-          };
+      mkHomeConfiguration = system: modules: homeDirectory:
+        home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.${system};
+          modules = [
+            ./home-manager/home.nix
+          ] ++ modules ++ [
+            {
+              home.username = "tobias";
+              home.homeDirectory = homeDirectory;
+              home.stateVersion = "24.05";
+            }
+          ];
         };
+    in
+    {
+      # Home Manager configurations must be top-level so `.#linux` resolves.
+      homeConfigurations = {
+        linux = mkHomeConfiguration "x86_64-linux" [ ./hosts/linux/home.nix ] "/home/tobias";
+        darwin = mkHomeConfiguration "aarch64-darwin" [ ./hosts/darwin/home.nix ] "/Users/tobias";
+      };
 
-        # Development shell for working on den itself
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
+      # Keep development tools available for each host system.
+      devShells = flake-utils.lib.eachSystem supportedSystems (system: {
+        default = (nixpkgs.legacyPackages.${system}).mkShell {
+          buildInputs = with nixpkgs.legacyPackages.${system}; [
             nixpkgs-fmt
             nix-update
             home-manager
           ];
         };
+      });
 
-        formatter.default = pkgs.nixpkgs-fmt;
-      }
-    );
+      formatter = flake-utils.lib.eachSystem supportedSystems (system:
+        nixpkgs.legacyPackages.${system}.nixpkgs-fmt
+      );
+    };
 }
